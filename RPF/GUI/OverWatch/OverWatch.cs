11f9fdc69df778087dd29db596825a87bd450158
MRCAT9898 <luca.cisternino.carovigno@gmail.com>
2025-12-13 22:54:59 +0100

    Fixed the overwatch problem null exception.

--- RPF/GUI/OverWatch/OverWatch.cs
+++ RPF/GUI/OverWatch/OverWatch.cs
@@ -1,46 +1,59 @@
 ﻿using System.Linq;
 using Exiled.API.Features;
 using Exiled.Events.EventArgs.Player;
-using LabApi.Events.Arguments.PlayerEvents;
 using PlayerRoles;
 using RueI.API;
 using RueI.API.Elements;
 
-namespace RPF.GUI.OverWatch;
-
-/// <summary>
-/// Added an OverWatch GUI that shows player information when they become Overwatch.
-/// </summary>
-public class Overwatch
+namespace RPF.GUI.OverWatch
 {
-    private void OnChangingRole(ChangingRoleEventArgs ev)
+    /// <summary>
+    /// Added an OverWatch GUI that shows player information when they become Overwatch.
+    /// </summary>
+    public class Overwatch
     {
-        RueDisplay display = RueDisplay.Get(ev.Player);
-        Tag timerTag = new Tag("Timer");
-        Player player = Player.Get(ev.Player.CurrentSpectatingPlayers.FirstOrDefault());
-        
-        Log.Info("[RPF - CustomGUI]: Player changing role at overwatch detected.");
-        if (ev.NewRole == RoleTypeId.Overwatch)
+        private void OnChangingRole(ChangingRoleEventArgs ev)
         {
-            DynamicElement dynamicTimer = new DynamicElement(180, () =>
+            RueDisplay display = RueDisplay.Get(ev.Player);
+            Tag timerTag = new Tag("Timer");
+
+            var spectatedApiPlayer = ev.Player.CurrentSpectatingPlayers.FirstOrDefault();
+            if (spectatedApiPlayer == null)
             {
-                string overwatch = "<color=blue>OverWatch System:</color>\n" +
-                                   $"NickName: {player.Nickname}\n" +
-                                   $"ID: {ev.Player.Id}\n" +
-                                   $"Inventory: \n{player.Inventory}";
-                return overwatch;
-            });
-            display.Show(timerTag, dynamicTimer);
-            return;
+                Log.Warn("[RPF - CustomGUI]: Nessun giocatore osservato.");
+                display.Remove(timerTag);
+                return;
+            }
+
+            Player player = Player.Get(spectatedApiPlayer);
+
+            Log.Info("[RPF - CustomGUI]: Player changing role at overwatch detected.");
+
+            if (ev.NewRole == RoleTypeId.Overwatch)
+            {
+                DynamicElement dynamicTimer = new DynamicElement(180, () =>
+                {
+                    string overwatch = "<color=blue>OverWatch System:</color>\n" +
+                                       $"NickName: {player.Nickname}\n" +
+                                       $"ID: {player.Id}\n" +
+                                       $"Inventory: {player.Inventory}";
+                    return overwatch;
+                });
+                display.Show(timerTag, dynamicTimer);
+                return;
+            }
+
+            display.Remove(timerTag);
         }
-        display.Remove(timerTag);
-    }
-    public void Register()
-    {
-        Exiled.Events.Handlers.Player.ChangingRole += OnChangingRole;
-    }
-    public void Unregister()
-    {
-        Exiled.Events.Handlers.Player.ChangingRole -= OnChangingRole;
+
+        public void Register()
+        {
+            Exiled.Events.Handlers.Player.ChangingRole += OnChangingRole;
+        }
+
+        public void Unregister()
+        {
+            Exiled.Events.Handlers.Player.ChangingRole -= OnChangingRole;
+        }
     }
 }
